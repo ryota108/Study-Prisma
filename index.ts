27f7b2938d21574d8da7e8d5e8218775b25abca2
Ryota <aya.ryo.718@outlook.jp>
2023-06-13 23:16:38 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,93 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany({
+    include: { Posts: true },
+  });
+  return res.json(users);
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const user = await prisma.user.findUnique({
+    include: { Posts: true },
+    where: {
+      id,
+    },
+  });
+  return res.json(user);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const { name } = req.body;
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+app.post("/posts", async (req: Request, res: Response) => {
+  const { title, content, authorId } = req.body;
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId,
+      },
+    });
+    return res.json(post);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
